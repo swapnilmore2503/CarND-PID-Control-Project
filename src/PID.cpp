683895e770dc8796cb40c69793fd838b0230456a
Shruti More <shruti@Shrutis-MacBook-Pro.local>
2018-01-22 10:45:32 +0530

    First Commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+#include <stdlib.h>
 #include "PID.h"
+#define intervallen 25
 
 using namespace std;
 
@@ -11,6 +14,16 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+    
+    interval = (int*) calloc(intervallen, sizeof(interval[0]));
+    i = intervallen - 1;
+    sum = 0;
 }
 
 void PID::UpdateError(double cte) {
--- src/main.cpp
+++ src/main.cpp
@@ -30,48 +30,54 @@ std::string hasData(std::string s) {
 
 int main()
 {
-  uWS::Hub h;
-
-  PID pid;
-  // TODO: Initialize the pid variable.
-
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    uWS::Hub h;
+    PID pid;
+    // TODO: Initialize the pid variable.
+    time = 0.0; // Placeholder for current time
+    pre_time = clock(); // Previous time
+    double total_time = 0.0; // Total time
+    pid.Init(0.3, 0.01, 0.004);
+    
+    h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-      auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
-        auto j = json::parse(s);
-        std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
+        if (length && length > 2 && data[0] == '4' && data[1] == '2')
+        {
+         auto s = hasData(std::string(data).substr(0, length));
+         if (s != "") {
+           auto j = json::parse(s);
+           std::string event = j[0].get<std::string>();
+           if (event == "telemetry") {
           // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
+             double cte = std::stod(j[1]["cte"].get<std::string>());
+             double speed = std::stod(j[1]["speed"].get<std::string>());
+             double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+             double steer_value;
+            /*
+            * TODO: Calcuate steering value here, remember the steering value is
+            * [-1, 1].
+            * NOTE: Feel free to play around with the throttle and speed. Maybe use
+            * another PID controller to control the speed!
+            */
+               time = clock();
+               double dt = (time - pre_time) / CLOCK_PER_SEC;
+            
           
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+            std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = 0.3;
+            auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+            std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+    }   else {
+            // Manual driving
+            std::string msg = "42[\"manual\",{}]";
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }
   });
